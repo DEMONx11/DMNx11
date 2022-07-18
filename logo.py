2ac4e611f987390081bc013b27d803cc19a6201a
DEMONx11 <109507443+DEMONx11@users.noreply.github.com>
2022-07-18 17:32:45 +0800

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,31 @@
+################### 
+ ## ANAK BABI KAU ## 
+ ################### 
+ ## WARNA ASU ##### 
+ P = '\x1b[1;97m' # PUTIH 
+ M = '\x1b[1;91m' # MERAH 
+ H = '\x1b[1;92m' # HIJAU 
+ K = '\x1b[1;93m' # KUNING 
+ B = '\x1b[1;94m' # BIRU 
+ U = '\x1b[1;95m' # UNGU 
+ O = '\x1b[1;96m' # BIRU MUDA 
+ N = '\x1b[0m'    # WARNA MATI 
+ ################## 
+ sayang_april=(""" 
+ \x1b[1;92m╔═══════[  \x1b[1;91mDEMO1;92m]═════════════════════════════╗ 
+ \x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mAuthor : DEMONx11                        \x1b[1;92m■ 
+ \x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mFacbook : Sapa Yah                      \x1b[1;92m■ 
+ \x1b[1;92m■  \x1b[1;93m[▪]   \x1b[1;94mGithub : github.com/DEMONx11             \x1b[1;92m■ 
+ \x1b[1;92m╚═══════[   XXXXXX ]═════════════════════════════╝ 
+ """) 
+  
+ import itertools 
+ import threading 
+ import time 
+ import sys 
+ import os 
+  
+ inu =print 
+ def kntl(): 
+       os.system('clear') 
+ inu (sayang_april)
---
+++ tools.py
@@ -0,0 +1,60 @@
+###################### 
+ ## WARNA ASU ##### WARNA ## 
+ P = '\x1b[1;97m' # PUTIH ####### 
+ M = '\x1b[1;91m' # MERAH ###### 
+ H = '\x1b[1;92m' # HIJAU ###### 
+ K = '\x1b[1;93m' # KUNING #### 
+ B = '\x1b[1;94m' # BIRU ###### 
+ U = '\x1b[1;95m' # UNGU ###### 
+ O = '\x1b[1;96m' # BIRU MUDA ### 
+ N = '\x1b[0m'      # WARNA MATI ### 
+ ###################### 
+ april =input 
+ import random 
+ import sys 
+ import time 
+ import os 
+ def inu_ganteng_banget(s): 
+     for c in s + '\n': 
+         sys.stdout.write(c) 
+         sys.stdout.flush() 
+         time.sleep(random.random() * 0.1) 
+  
+  
+ user_reply = april("\x1b[1;92mWhat Your Name? \x1b[1;93m") 
+ inu_ganteng_banget('\x1b[1;94m[+]===============[ MENU ]===============[+]') 
+ inu_ganteng_banget('\x1b[1;96m1 \x1b[1;92mInstall material') 
+ inu_ganteng_banget('\x1b[1;96m2 \x1b[1;92mVersi 1') 
+ inu_ganteng_banget('\x1b[1;96m3 \x1b[1;92mVersi 2') 
+ inu_ganteng_banget('\x1b[1;96m4 \x1b[1;92mVersi 3') 
+ inu_ganteng_banget('\x1b[1;96m5\x1b[1;92mVersi 4') 
+ inu_ganteng_banget('\x1b[1;96m6\x1b[1;92mVersi 5') 
+ inu_ganteng_banget('\x1b[1;96m7 \x1b[1;92mSpamcall') 
+ inu_ganteng_banget('\x1b[1;96m8 \x1b[1;92mSpam By Bang Xenzi') 
+ inu_ganteng_banget('\x1b[1;96m9 \x1b[1;92mDB-BOM Spam By Bang Xenzi') 
+ inu_ganteng_banget('\x1b[1;96m10 \x1b[1;92mSpamer') 
+ inu_ganteng_banget('\x1b[1;96m11 \x1b[1;92mRed Hawk') 
+ inu_ganteng_banget('\x1b[1;96m12 \x1b[1;92mD-Tect') 
+ inu_ganteng_banget('\x1b[1;96m13 \x1b[1;92mHunner') 
+ inu_ganteng_banget('\x1b[1;96m14 \x1b[1;92mWPScan') 
+ inu_ganteng_banget('\x1b[1;96m15 \x1b[1;92mWebdav') 
+ inu_ganteng_banget('\x1b[1;96m16\x1b[1;92m Metasploit') 
+ inu_ganteng_banget('\x1b[1;96m17\x1b[1;92m Kali Nethunter') 
+ inu_ganteng_banget('\x1b[1;96m18\x1b[1;92m Ubuntu') 
+ inu_ganteng_banget('\x1b[1;96m19 \x1b[1;92mYoutube Dl') 
+ inu_ganteng_banget('\x1b[1;96m20 \x1b[1;92mviSQL') 
+ inu_ganteng_banget('\x1b[1;96m21 \x1b[1;92mWeeman') 
+ inu_ganteng_banget('\x1b[1;96m22 \x1b[1;92mWFDroid') 
+ inu_ganteng_banget('\x1b[1;96m23\x1b[1;92m FBBrute') 
+ inu_ganteng_banget('\x1b[1;96m24 \x1b[1;92mNgrok') 
+ inu_ganteng_banget('\x1b[1;96m25\x1b[1;92m Torshammer') 
+ inu_ganteng_banget('\x1b[1;96m26\x1b[1;92m RouterSploit') 
+ inu_ganteng_banget('\x1b[1;96m27\x1b[1;92m Hydra') 
+ inu_ganteng_banget('\x1b[1;96m28 \x1b[1;92mWeevely') 
+ inu_ganteng_banget('\x1b[1;96m29 \x1b[1;92mSQLMap') 
+ inu_ganteng_banget('\x1b[1;96m30\x1b[1;92m Dirbuster') 
+ inu_ganteng_banget('\x1b[1;96m31 \x1b[1;92mPybelt') 
+ inu_ganteng_banget('\x1b[1;96m00 \x1b[1;91mEXIT TO MENU') 
+ inu_ganteng_banget('\x1b[1;94m[+]===============[ MENU ]===============[+]') 
+ print("\x1b[1;93mBang", user_reply) 
+ inu_ganteng_banget('\x1b[0mselect menu')
