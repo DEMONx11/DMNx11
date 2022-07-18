35d0e7c31beae90abc4546da0c98f542c4d9a4e1
DEMONx11 <109507443+DEMONx11@users.noreply.github.com>
2022-07-18 15:45:34 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,532 @@
+#!/system/xbin/bash/sh/python 
+ MIT License 
+  
+ Copyright (c) 2022 DEMONx11 
+  
+ Permission is hereby granted, free of charge, to any person obtaining a copy 
+ of this software and associated documentation files (the "Software"), to deal 
+ in the Software without restriction, including without limitation the rights 
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ copies of the Software, and to permit persons to whom the Software is 
+ furnished to do so, subject to the following conditions: 
+  
+ The above copyright notice and this permission notice shall be included in all 
+ copies or substantial portions of the Software. 
+  
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ SOFTWARE. 
+ """ 
+  
+  
+ cd module 
+ clear 
+ python loading.py 
+ sleep 3 
+ clear 
+ echo "Follow my Facebook >.<" 
+ xdg-open https://www.facebook.com/profile.php?id=100078199765906 
+ sleep 3 
+ clear 
+ python logo.py 
+ python tools.py 
+  
+ biru='\033[34;1m' 
+ ijo='\033[32;1m'   
+ ungu='\033[35;1m' 
+ bm='\033[36;1m' 
+ abang='\033[31;1m' 
+ putih='\033[37;1m'                                            
+ kuning='\033[33;1m' 
+ sleep 1 
+  
+  
+ read -p "Pilih No:" bro 
+  
+ ## MENU1 
+ if [ $bro = 1 ] || [ $bro = 1 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ pkg update && pkg upgrade 
+  
+ pkg install python python2 python3 
+  
+ pip install requests mechanize 
+  
+ pip2 install requests mechanize 
+  
+ pkg install git 
+  
+ pkg install ruby 
+  
+ pkg install curl 
+  
+ gem install lolcat 
+  
+ pip2 install php 
+  
+ pkg install figlet 
+  
+ pkg install wget 
+  
+ pkg install nano 
+  
+ pkg install bash 
+  
+ pkg install cowsay 
+  
+ apt update && apt upgrade -y 
+  
+ pkg install bash 
+  
+ pkg install pv -y 
+  
+ pkg install wget -y 
+  
+ pkg install git -y 
+  
+ bash run.sh 
+ fi 
+  
+ ## Menu2 
+ if [ $bro = 2 ] || [ $bro = 2 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/THEMOON555/SPM-x1
+ cd SpXv1  
+ python SPM-x1
+ fi 
+  
+ ## MENU3 
+ if [ $bro = 3 ] || [ $bro = 3 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/THEMOON555/XD-LOM
+ cd XD-LOM
+ npm i
+ node index
+ fi 
+  
+ ## MENU4 
+ if [ $bro = 4 ] || [ $bro = 4 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/THEMOON555/yaaa
+ cd yaaa
+ npm i
+ node index
+ fi 
+  
+  
+ ## MENU5 
+ if [ $bro = 5 ] || [ $bro = 5 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ pkg update 
+ pkg upgrade 
+ pkg install git 
+ clear 
+ git clone https://github.com/THEMOON555/Haaa-
+ cd Haaa-
+ npm i
+ node index
+ fi 
+  
+ ## MENU6 
+ if [ $bro = 6 ] || [ $bro = 6 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/MR-Xyaa/terkey 
+ cd terkey 
+ git pull 
+ python terkey.py 
+ fi 
+  
+ ## MENU7 
+ if [ $bro = 7 ] || [ $bro = 7 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/MR-Xyaa/spamcall 
+ cd spamcall 
+ git pull 
+ sh spam-call.sh 
+ fi 
+  
+ ## MENU8 
+ if [ $bro = 8 ] || [ $bro = 8 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/Xenzi-XN1/Tools-Spam 
+ cd Tools-Spam 
+ git pull 
+ python Menu.py 
+ fi 
+  
+ ## MENU0 
+ if [ $bro = 00 ] || [ $bro = 00 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ sleep 1 
+ echo -e $abang"We Security" 
+ sleep 1 
+ echo -e $biru"We Not Friends" 
+ sleep 1 
+ echo -e $bm"We Are Family" 
+ sleep 1 
+ echo -e $kuning"Hacking Is Not Criminal" 
+ sleep 1 
+ echo -e $ungu"Ketika Sebuah Hayalan Tidak tercapai" 
+ sleep 1 
+ echo -e $putih"Maka Terus lah BerJuang Dan Berusaha" 
+ sleep 1 
+ echo -e $kuning"Karna Suatu Hari Nanti Kamu akan Mendapatkannya" 
+ sleep 1 
+ echo -e $ijo"DEMONx11" 
+ sleep 5 
+ python tai.py 
+ sleep 5 
+ clear 
+ exit 
+ fi 
+  
+ # MENU9 
+ if [ $bro = 9 ] || [ $bro = 9 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ pkg update && pkg upgrade 
+ pkg install nano 
+ pkg install git 
+ pkg install python 
+ pip install requests 
+ git clone https://github.com/Xenzi-XN1/DB-Bom 
+ cd DB-Bom 
+ python DB.py 
+ fi 
+  
+ ## MENU10 
+  
+ if [ $bro = 10 ] || [ $bro = 10 ] 
+ then 
+ clear 
+ toilet -f slant "DEMONx11"; 
+ git clone https://github.com/MR-Xyaa/Spamer 
+ cd Spamer 
+ git pull 
+ bash mulai.sh 
+ fi 
+  
+ if [ $bro = 11 ] || [ $bro = 11 ] 
+ then 
+ clear 
+ echo -e $red" Installing Red Hawk " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install php 
+ apt install git 
+ git clone https://github.com/Tuhinshubhra/RED_HAWK 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 12 ] || [ $bro = 12 ] 
+ then 
+ clear 
+ echo -e $red" Installing D-Tect " 
+ sleep 1 
+ apt-get update && apt-get upgrade 
+ apt-get install git 
+ apt-get install python2 
+ git clone https://github.com/shawarkhanethicalhacker/D-TECT 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 13 ] || [ $bro = 13 ] 
+ then 
+ clear 
+ echo -e $red" Installing Hunner " 
+ sleep 1 
+ apt-get update && apt-get upgrade 
+ apt install python 
+ apt install git 
+ git clone https://github.com/b3-v3r/Hunner 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 14 ] || [ $bro = 14 ] 
+ then 
+ clear 
+ echo -e $red" Installing Wpscan " 
+ sleep 1 
+ apt-get update && apt-get upgrade 
+ apt install ruby 
+ apt install curl 
+ apt install git 
+ git clone https://github.com/wpscanteam/wpscan 
+ cd ~/wpscan 
+ gem install bundle 
+ bundle config build.nokogiri --use-system-libraries 
+ bundle install 
+ ruby wpscan.rb --update 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 15 ] || [ $bro = 15 ] 
+ then 
+ clear 
+ echo -e $red" Installing Webdav " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install python2 
+ pip2 install urllib3 chardet certifi idna requests 
+ apt install openssl curl 
+ pkg install libcurl 
+ mkdir webdav 
+ cd ~/webdav 
+ wget https://pastebin.com/raw/HnVyQPtR -O webdav.py 
+ chmod 777 webdav.py 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 16 ] || [ $bro = 16 ] 
+ then 
+ clear 
+ echo -e $red" Installing Metasploit " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install git 
+ apt install wget 
+ wget https://raw.githubusercontent.com/verluchie/termux-metasploit/master/install.sh 
+ chmod 777 install.sh 
+ sh install.sh 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 17 ] || [ $bro = 17 ] 
+ then 
+ clear 
+ echo -e $red" Installing Kali Nethunter " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install git 
+ git clone https://github.com/Hax4us/Nethunter-In-Termux.git 
+ cd ~/Nethunter-In-Termux 
+ chmod 777 kalinethunter 
+ sh kalinethunter 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 18 ] || [ $bro = 18 ] 
+ then 
+ clear 
+ echo -e $red" Installing Ubuntu " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install git 
+ apt install wget 
+ apt install proot 
+ git clone https://github.com/Neo-Oli/termux-ubuntu.git 
+ cd ~/termux-ubuntu 
+ chmod +x ubuntu.sh 
+ sh ubuntu.sh 
+ echo " Fix network please wait " 
+ sleep 1 
+ echo "nameserver 8.8.8.8" > /data/data/com.termux/files/home/termux-ubuntu/ubuntu-fs/etc/resolv.conf 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 19 ] || [ $bro = 19 ] 
+ then 
+ clear 
+ echo -e $red" Installing Youtube DL " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install python 
+ pip3 install mps_youtube 
+ pip3 install youtube_dl 
+ apt install mpv 
+ echo " Untuk menjalankannya ketik "mpsyt" tanpa tanda petik " 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 20 ] || [ $bro = 20 ] 
+ then 
+ clear 
+ echo -e $red" Installing viSQL " 
+ sleep 1 
+ apt update && apt upgrade 
+ pkg install git 
+ pkg install python2 
+ git clone https://github.com/blackvkng/viSQL.git 
+ cd ~/viSQL 
+ chmod 777 viSQL.py 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 21 ] || [ $bro = 21 ] 
+ then 
+ clear 
+ echo -e $red" Installing Weeman " 
+ sleep 1 
+ apt update && apt upgrade 
+ pkg install git 
+ apt install python2 
+ git clone https://github.com/samyoyo/weeman 
+ cd ~/weeman 
+ pip2 install beautifulsoup 
+ pip2 install bs4 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 22 ] || [ $bro = 22 ] 
+ then 
+ clear 
+ echo -e $red" Installing WFDroid " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install wget 
+ mkdir wfdroid 
+ cd ~/wfdroid 
+ wget https://raw.githubusercontent.com/bytezcrew/wfdroid-termux/master/wfdinstall 
+ chmod 777 wfdinstall 
+ sh wfdinstall 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 23 ] || [ $bro = 23 ] 
+ then 
+ clear 
+ sleep 1 
+ echo -e $red" Installing FBBrute " 
+ apt install python2 
+ apt install python2-dev 
+ apt install wget 
+ pip2 install mechanize 
+ mkdir fbbrute 
+ cd ~/fbbrute 
+ wget https://pastebin.com/raw/aqMBt2xA -O fbbrute.py 
+ chmod 777 fbbrute.py 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 24 ] || [ $bro = 24 ] 
+ then 
+ clear 
+ echo -e $red" Installing Ngrok " 
+ sleep 1 
+ apt install wget 
+ mkdir ngrok 
+ cd ~/ngrok 
+ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip 
+ unzip ngrok-stable-linux-arm.zip 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 25 ] || [ $bro = 25 ] 
+ then 
+ clear 
+ echo -e $red" Installing Hammer " 
+ sleep 1 
+ pkg update 
+ pkg upgrade 
+ pkg install python 
+ pkg install git 
+ git clone https://github.com/cyweb/hammer 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 16 ] || [ $bro = 26 ] 
+ then 
+ clear 
+ echo -e $red" Installing Routersploit " 
+ sleep 1 
+ apt install git 
+ apt install python2 
+ pip2 install requests 
+ git clone https://github.com/reverse-shell/routersploit.git 
+ cd routersploit 
+ pip install -r requirements.txt 
+ termux-fix-shebang rsf.py 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 17 ] || [ $bro = 27 ] 
+ then 
+ clear 
+ echo -e $red" Installing Hydra " 
+ sleep 1 
+ apt update && apt install -y wget 
+ apt install hydra 
+ wget http://scrapmaker.com/download/data/wordlists/dictionaries/rockyou.txt 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 28 ] || [ $bro = 28 ] 
+ then 
+ clear 
+ echo -e $red" Installing Weevely " 
+ sleep 1 
+ pkg update 
+ pkg upgrade 
+ git clone https://github.com/glides/Weevely 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 29 ] || [ $bro 29 ] 
+ then 
+ clear 
+ echo -e $red" Installing SQLMap " 
+ sleep 1 
+ apt update && apt upgrade 
+ apt install python2 
+ git clone https://github.com/sqlmapproject/sqlmap.git 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 30 ] || [ $bro = 30 ] 
+ then 
+ clear 
+ echo -e $red" Installing Dirbuster " 
+ sleep 1 
+ apt-get update 
+ apt-get install python 
+ apt-get install git 
+ git clone https://github.com/maurosoria/dirsearch.git 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi 
+  
+ if [ $bro = 31 ] || [ $bro = 31 ] 
+ then 
+ clear 
+ echo -e $red" Installing Pybelt " 
+ sleep 1 
+ git clone https://github.com/ekultek/pybelt.git 
+ pip install –r requirements.txt 
+ cd ~/ 
+ echo -e $red" T E R I N S T A L L " 
+ fi
